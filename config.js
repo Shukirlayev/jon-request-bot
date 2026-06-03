1902d7f379049c7bf060b4f5dc40af4ac34b28a1
Sarvar Shukirlayev <Shukirlayevs@gmail.com>
2026-06-03 16:51:41 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
     BOT_TOKEN: process.env.BOT_TOKEN,
     ADMIN_ID: parseInt(process.env.ADMIN_ID, 10),
     GROUP_ID: process.env.GROUP_ID,
+    CHANNEL_ID: process.env.CHANNEL_ID, // Majburiy a'zolik uchun kanal
     PORT: process.env.PORT || 3000
 };
