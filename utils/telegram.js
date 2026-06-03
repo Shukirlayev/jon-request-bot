b7eb15fe669665c5d3139fbaef089dc90917fad0
Sarvar Shukirlayev <Shukirlayevs@gmail.com>
2026-06-03 16:57:37 +0500

    Update telegram.js

--- utils/telegram.js
+++ utils/telegram.js
@@ -1,6 +1,10 @@
 // Duplicate requestlarni saqlash uchun cache
 const requestCache = new Map();
 
+// Tasdiqdan o'tmagan foydalanuvchilarni vaqtincha saqlash uchun cache
+const pendingVerifications = new Map();
+
 module.exports = {
-    requestCache
+    requestCache,
+    pendingVerifications
 };
